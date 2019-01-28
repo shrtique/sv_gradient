8cd59898d370602bb94f8ff54f0544f8eef13db1
shrtique <shrt@d-test.ru>
2019-01-28 17:47:40 +0500

    another try to init

---
+++ sources_1/new/M_AXIS_module.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/28/2019 05:42:57 PM
+// Design Name: 
+// Module Name: M_AXIS_module
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module M_AXIS_module(
+
+    );
+endmodule
---
+++ sources_1/new/processing_Gx_Gy.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/28/2019 05:43:43 PM
+// Design Name: 
+// Module Name: processing_Gx_Gy
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module processing_Gx_Gy(
+
+    );
+endmodule
