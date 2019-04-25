0cd38f05e3ea8cfbd5f7373ec43ba1fae866bec3
shrtique <shrt@d-test.ru>
2019-04-25 12:11:20 +0500

    added one more stage to pipline

--- sources_1/new/processing_Gx_Gy.sv
+++ sources_1/new/processing_Gx_Gy.sv
@@ -35,6 +35,7 @@ module processing_Gx_Gy #(
 //
 //////////////////////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////////////////////////////////
 // Here we add input stage for image data, cause i_image_kernel_buffer has relatively large fanout for high speed design
 // Let's try
 
@@ -47,8 +48,10 @@ always_ff @( posedge i_clk, negedge i_aresetn ) begin
     image_kernel_buffer <= i_image_kernel_buffer;
   end
 end
+//////////////////////////////////////////////////////////////////////////////////
 
 
+//////////////////////////////////////////////////////////////////////////////////
 //SUMS Gx_cols
 
 logic signed [DATA_WIDTH+4:0] Gx_col [0:3]; // one col with zeros we miss
@@ -65,7 +68,20 @@ always_ff @( posedge i_clk, negedge i_aresetn ) begin
   end 
 end 
 
+//reg input data once again, for pipelining calculations of Gx and Gy
+logic [DATA_WIDTH-1:0] image_kernel_buffer_reg [0:KERNEL_SIZE-1] [0:KERNEL_SIZE-1];
 
+always_ff @( posedge i_clk, negedge i_aresetn ) begin 
+  if ( ~i_aresetn ) begin
+    image_kernel_buffer_reg <= '{default: '0};
+  end else begin
+    image_kernel_buffer_reg <= image_kernel_buffer;
+  end
+end
+//////////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////////////
 //SUMS Gy_rows
 logic signed [DATA_WIDTH+4:0] Gy_row [0:3]; // one row with zeros we miss
 
@@ -74,14 +90,27 @@ always_ff @( posedge i_clk, negedge i_aresetn ) begin
   if ( ~i_aresetn ) begin
     Gy_row <= '{default: 'b0};
   end else begin
-    Gy_row[0] <=   ( ( image_kernel_buffer[0][0] + image_kernel_buffer[0][1] + image_kernel_buffer[0][2] + image_kernel_buffer[0][3] + image_kernel_buffer[0][4] ) << 1 );
-    Gy_row[1] <=     ( image_kernel_buffer[1][0] + image_kernel_buffer[1][1] + image_kernel_buffer[1][2] + image_kernel_buffer[1][3] + image_kernel_buffer[1][4] );
-    Gy_row[2] <= -   ( image_kernel_buffer[3][0] + image_kernel_buffer[3][1] + image_kernel_buffer[3][2] + image_kernel_buffer[3][3] + image_kernel_buffer[3][4] );
-    Gy_row[3] <= - ( ( image_kernel_buffer[4][0] + image_kernel_buffer[4][1] + image_kernel_buffer[4][2] + image_kernel_buffer[4][3] + image_kernel_buffer[4][4] ) << 1 );
+    Gy_row[0] <=   ( ( image_kernel_buffer_reg[0][0] + image_kernel_buffer_reg[0][1] + image_kernel_buffer_reg[0][2] + image_kernel_buffer_reg[0][3] + image_kernel_buffer_reg[0][4] ) << 1 );
+    Gy_row[1] <=     ( image_kernel_buffer_reg[1][0] + image_kernel_buffer_reg[1][1] + image_kernel_buffer_reg[1][2] + image_kernel_buffer_reg[1][3] + image_kernel_buffer_reg[1][4] );
+    Gy_row[2] <= -   ( image_kernel_buffer_reg[3][0] + image_kernel_buffer_reg[3][1] + image_kernel_buffer_reg[3][2] + image_kernel_buffer_reg[3][3] + image_kernel_buffer_reg[3][4] );
+    Gy_row[3] <= - ( ( image_kernel_buffer_reg[4][0] + image_kernel_buffer_reg[4][1] + image_kernel_buffer_reg[4][2] + image_kernel_buffer_reg[4][3] + image_kernel_buffer_reg[4][4] ) << 1 );
   end 
 end 
 
 
+logic signed [DATA_WIDTH+4:0] Gx_col_reg [0:3];
+
+always_ff @( posedge i_clk, negedge i_aresetn ) begin 
+  if ( ~i_aresetn ) begin
+    Gx_col_reg <= '{default: '0};
+  end else begin
+    Gx_col_reg <= Gx_col;
+  end
+end
+//////////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////////////
 //SUMS Gx, SUMS Gy
 logic signed [DATA_WIDTH+4:0] Gx;
 logic signed [DATA_WIDTH+4:0] Gy;
@@ -92,12 +121,13 @@ always_ff @( posedge i_clk, negedge i_aresetn ) begin
     Gx <= '{default: 'b0};
     Gy <= '{default: 'b0};
   end else begin
-    Gx <= Gx_col[0] + Gx_col[1] + Gx_col[2] + Gx_col[3];
+    Gx <= Gx_col_reg[0] + Gx_col_reg[1] + Gx_col_reg[2] + Gx_col_reg[3];
     Gy <= Gy_row[0] + Gy_row[1] + Gy_row[2] + Gy_row[3];
   end 
 end
+//////////////////////////////////////////////////////////////////////////////////
 
-
+//////////////////////////////////////////////////////////////////////////////////
 //
 always_ff @( posedge i_clk, negedge i_aresetn ) begin
   if ( ~i_aresetn ) begin
@@ -107,14 +137,14 @@ always_ff @( posedge i_clk, negedge i_aresetn ) begin
     o_Gx_Gy_vector [31:16] <= Gy;
   end 
 end
+//////////////////////////////////////////////////////////////////////////////////
 
 
-
-//delay sof and valid to sync with next module (delay = 4 clk ticks, same as delay for Gx Gy processing)
+//delay sof and valid to sync with next module (delay = 5 clk ticks, same as delay for Gx Gy processing)
 //
 //SIGNALS
-logic delay_buffer_valid[0:2];
-logic delay_buffer_sof[0:2];
+logic delay_buffer_valid[0:3];
+logic delay_buffer_sof[0:3];
 
 /*
 always_ff @( posedge i_clk, negedge i_aresetn )
@@ -142,11 +172,11 @@ always_ff @( posedge i_clk, negedge i_aresetn )
  always_ff @( posedge i_clk )
   begin
 
-    delay_buffer_valid <= {i_data_valid, delay_buffer_valid[0:1]};
-    delay_buffer_sof   <= {i_start_of_frame, delay_buffer_sof[0:1]};
+    delay_buffer_valid <= {i_data_valid, delay_buffer_valid[0:2]};
+    delay_buffer_sof   <= {i_start_of_frame, delay_buffer_sof[0:2]};
        
-    o_data_valid       <= delay_buffer_valid[2];
-    o_start_of_frame   <= delay_buffer_sof[2];
+    o_data_valid       <= delay_buffer_valid[3];
+    o_start_of_frame   <= delay_buffer_sof[3];
            	
   end
 
